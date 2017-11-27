b191e9c900c5b6096132b441bb105b0c54977661
TebogoP <tphiri.firi@gmail.com>
2017-11-27 05:13:14 -0500

    I'm adding DisplayTen.java file and DisplayTen.class file

---
+++ DisplayTen.java
@@ -0,0 +1,19 @@
+class DisplayTen{
+public DisplayTen()
+{}
+
+void display(int n){
+ for (int i=0 ; i <n;i++){
+	System.out.println(i );
+ }
+
+}
+
+public static void main(String asd[]){
+	DisplayTen bj= new DisplayTen();
+	
+	bj.display(10);
+}
+
+
+}
